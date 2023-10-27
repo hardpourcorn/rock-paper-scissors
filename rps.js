6c171dac86e7e024c46ea20fb0413fc457082937
hardpourcorn <montenegroc17@gmail.com>
2023-10-26 22:50:01 -0400

    Add playRound function comparing choices

--- rps.js
+++ rps.js
@@ -1,9 +1,9 @@
 // Game starts with the decisions that computer can pick
-
+const choices = ["rock", "paper", "scissors"];
 let playerCount = 0;
 let compCount = 0;
 
-const choices = ["rock", "paper", "scissors"]; 
+
 
 
 // Ask player for input and compare to premade array to accuracy
@@ -11,21 +11,41 @@ let getPlayerChoice = prompt ("Please select rock, paper, or scissors");
   if (getPlayerChoice === choices) {
         console.log(getPlayerChoice);
 }
-playerSelection = getPlayerChoice;
+const playerSelection = getPlayerChoice;
 
-
+// Get Random choice for computer
 function getComputerChoice() {
     return choices[(Math.floor(Math.random() * choices.length))];
 }
-compSelection = getComputerChoice();
+const compSelection = getComputerChoice();
+
 
 
 // Compare player choice to computer choice 
 function playRound(playerSelection, compSelection) {
-   if (playerSelection === compSelection) {
-    console.log("It's a tie!");
-   }
+    if (playerSelection === compSelection) {
+        console.log("It's a tie!");
+    }
+    else if (
+        (playerSelection == "rock" && compSelection == "scissors") ||
+        (playerSelection == "paper" && compSelection == "rock") ||
+        (playerSelection == "scissors" && compSelection == "paper")
+    ) {
+        console.log("Player Wins!");
+    }
+    else if (
+        (playerSelection == "rock" && compSelection == "paper") ||
+        (playerSelection == "paper" && compSelection == "scissors") ||
+        (playerSelection == "scissors" && compSelection == "rock")
+    ){
+        console.log("Computer wins!");
+    }
 }
+
+console.log(playRound(playerSelection, compSelection));
+
+
+// Having issues where computer has not won a single round
 
 // Create conditions to decide winner.
 
