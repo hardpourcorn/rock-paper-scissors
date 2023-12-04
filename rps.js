9ad79094f460f7ad40ae42d51d57740311fa911a
hardpourcorn <143234680+hardpourcorn@users.noreply.github.com>
2023-12-03 20:15:49 -0500

    Add bug comments, rm unnec. lines

--- rps.js
+++ rps.js
@@ -1,59 +1,50 @@
-// Game starts with the decisions that computer can pick
 const choices = ["rock", "paper", "scissors"];
-let playerCount = 0;
-let compCount = 0;
 let round = 1;
+let playerSelection = getPlayerChoice();
+let compSelection = getComputerChoice();
 
-
-let getPlayerChoice = prompt ("Please select rock, paper, or scissors");
-  if (getPlayerChoice === choices) {
-        console.log(getPlayerChoice);
-}
 function getComputerChoice() {
-    return choices[(Math.floor(Math.random() * choices.length) + 1)];
+    return choices[(Math.floor(Math.random() * choices.length))];
 }
 
-const playerSelection = getPlayerChoice;
-const compSelection = getComputerChoice();
+// getPlayerChoice will return as undefined. Will define but function call cannot find definition
 
-
+function getPlayerChoice() {
+    let playerChoice = prompt ("Please select rock, paper, or scissors");
+    if (playerChoice === choices) {
+        console.log(playerChoice);
+    }
+}
 
 function playRound(playerSelection, compSelection) {
     if (playerSelection === compSelection) {
-        console.log("It's a tie!");
+        return("It's a tie!");
     }
     else if (
         (playerSelection == "rock" && compSelection == "scissors") ||
         (playerSelection == "paper" && compSelection == "rock") ||
         (playerSelection == "scissors" && compSelection == "paper")
     ) {
-        console.log("Player Wins!");
+        return("Player Wins!");
     }
     else if (
         (playerSelection == "rock" && compSelection == "paper") ||
         (playerSelection == "paper" && compSelection == "scissors") ||
         (playerSelection == "scissors" && compSelection == "rock")
     ){
-        console.log("Computer wins!");
+        return("Computer wins!");
     }
 }
 
 function playGame() {
-    do {
-        prompt ("Please select rock, paper, or scissors");
-        playRound(playerSelection, compSelection);
-        console.log("Round " + round + " winner ");
+    do {getPlayerChoice();
+        getComputerChoice();
+        console.log(playRound());
         round++;
 }
 
 while (round < 5);
 }
 
+
 console.log(playGame());
-
-
-
-
-
-
-console.log(playGame(playRound()));
